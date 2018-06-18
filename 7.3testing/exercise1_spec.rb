effc0125928c6b27484cc131f82e6bd66ec10c67
Keenan Gebze <keenan.gebze@gmail.com>
2018-06-18 12:26:24 +0700

    Add one test case using splat operator..

--- 7.3testing/exercise1_spec.rb
+++ 7.3testing/exercise1_spec.rb
@@ -7,9 +7,14 @@ RSpec.describe Sentence do
       expect(Sentence.create_sentence).to eq("")
     end
     
-    it "Return hello ruby world" do
+    it "Return Hello Ruby World!" do
       param = ["Hello", "Ruby", "World!"]
       expect(Sentence.create_sentence(param)).to eq("Hello Ruby World!")
+    end
+    
+    it "Can be used using splat operator" do
+      param = ["Hello", "Ruby", "Test", "Splat", "World!"]
+      expect(Sentence.create_sentence(*param)).to eq("Hello Ruby Test Splat World!")
     end
     
   end
